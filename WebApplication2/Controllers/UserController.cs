b04838be768e719b434a1ac7eb5dc78a8a43546c
Iqdar Shah <iqdarshah@gmail.com>
2023-12-08 05:48:10 +0500

    Initial Commit

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace GerryTestPractice.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly MyDbContext _context;
+
+        public UserController(MyDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var user = _context.Users.ToList();
+            return View("Index", user);
+        }
+
+        public IActionResult AddUser()
+        {
+            User user = new User();
+            return View("UserForm", user);
+        }
+
+        public IActionResult EditUser(int id)
+        {
+            var user = _context.Users.SingleOrDefault( c => c.Id == id);
+            return View("UserForm", user);
+        }
+
+        public IActionResult Save(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UserForm", user);
+            }
+            if (user.Id == 0)
+            {
+                _context.Users.Add(user);
+                //return View("UserForm", user);
+            }
+            else
+            {
+                var userInDb = _context.Users.Single(c => c.Id == user.Id);
+                userInDb.Name = user.Name;
+                userInDb.Email = user.Email;    
+                userInDb.BirthDate = user.BirthDate;
+                user.Gender = user.Gender;
+                user.CreatedOn = userInDb.CreatedOn;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "User");
+        }
+
+
+    }
+}
---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        [AllowNull]
+        public DateTime CreatedOn { get; set; }
+
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
